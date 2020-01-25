6facc0f98079a8728ab913c063175bd151420c40
Dolet B <doletbizhev@gmail.com>
2020-01-26 01:01:32 +0300

    some component init

---
+++ components/theSectionDust/TheSectionDust.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    <h3>Dust</h3>
+  </section>
+</template>
+<script>
+export default {
+  name: 'TheSectionDust'
+}
+</script>
+<style lang="scss" scoped>
+section {
+  background-color: #ccc;
+}
+</style>
---
+++ pages/dust.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2>DUST</h2>
+    <the-section-dust />
+  </div>
+</template>
+<script>
+import TheSectionDust from '@/components/theSectionDust/TheSectionDust'
+export default {
+  components: {
+    TheSectionDust
+  }
+}
+</script>
+<style lang="scss" scoped>
+section {
+  background-color: #ccc;
+}
+</style>
