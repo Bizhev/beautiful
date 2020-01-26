3d5d709b7a6721c7930a0c9b98b3a65c349e8c69
Dolet B <doletbizhev@gmail.com>
2020-01-26 19:47:40 +0300

    add script deploy

---
+++ scripts/deploy_gh.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+repo="beautiful"
+dir="dist"
+autor="Bizhev"
+
+# STOP error
+set -e
+
+yarn build
+cd $dir
+
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# git push -f git@github.com:$autor/$autor.github.io.git master
+git push -f git@github.com:$autor/$repo.git master:gh-pages
+
+cd ..
+rm -rf $dir
